b54af57ab821aa1ab0d4a994ca8ea305371544cd
javadam <mrjavad833.2@gmail.com>
2025-04-04 17:47:19 +0330

    edit ta 1

--- src/main/java/org/example/test_hw2/HelloApplication.java
+++ src/main/java/org/example/test_hw2/HelloApplication.java
@@ -3,21 +3,21 @@ package org.example.test_hw2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         launch();
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
